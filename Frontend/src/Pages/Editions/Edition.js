ad998dcc1be313dd00a7be9871b56bbbda33bbcd
amirmejri96 <amirmejri1996@gmail.com>
2025-01-04 14:12:42 +0100

    edition

--- Frontend/src/Pages/Editions/Edition.js
+++ Frontend/src/Pages/Editions/Edition.js
@@ -5,7 +5,7 @@ const Edition = () => {
   return (
     <div>
         <header>
-        <h1 style={{color:'white'}} >LOCONADA - Les éditions passées</h1>
+        <h1 style={{color:'white'}} >LOCONADA - BEST OF EDITIONS</h1>
         <p>Découvrir les highlights des événements passées</p>
     </header>
 
@@ -14,14 +14,42 @@ const Edition = () => {
         {/* 1ère édition  */}
         <div className="editions">
         <div className='img' >
+            <img style={{width:'30%'}} src="/assets/LocoMolo7.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo8.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo9.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo10.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo11.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo12.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo13.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo14.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo4.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo6.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo5.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo1.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo3.jpg" alt="Loconada"/>
+            <img style={{width:'30%'}} src="/assets/LocoMolo2.jpg" alt="Loconada"/>
+            </div>
+            <div className="edition-content">
+                <p>Cette édition Loconada s'est déroulée le 29 décembre 2024 au Molo une boite de nuit située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
+                LINEUP : <br/> - Malko (Moblack Records) <br/> - Astrid<br/> - Ghassen Azaiez <br/> - Anis Houissa <br/> - Baros <br/> - Zoff </p>
+                <video controls>
+                    <source src="/assets/Vidéo LocoMolo.mp4" type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+        
+        
+        {/* 2ème édition  */}
+        <div className="editions">
+        <div className='img' >
             <img style={{width:'30%'}} src="/assets/Affiche Soulwhen.jpg" alt="Loconada"/>
             <img style={{width:'30%'}} src="/assets/1-Soulwhen.jpg" alt="Loconada"/>
             <img style={{width:'30%'}} src="/assets/2-Soulwhen.jpg" alt="Loconada"/>
             <img style={{width:'30%'}} src="/assets/3-Soulwhen.jpg" alt="Loconada"/>
             </div>
             <div className="edition-content">
-                <h2>Loconada 1ère édition</h2>
-                <p>La première édition Loconada s'est déroulée le 29 aout 2024 au Soulwhen Acqua Viva Hotel un rooftop située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
+                <p>Cette édition Loconada s'est déroulée le 29 aout 2024 au Soulwhen Acqua Viva Hotel un rooftop située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
                 LINEUP : <br/> - Adwas (Sol selectas - Cosmic Awakenings) <br/> - Al-Dee<br/> - Ghassen Azaiez <br/> - Anis Houissa <br/> - Draoui <br/> - Le Zak <br/> - Saif Touati <br/> - Youssef Naili </p>
                 <video controls>
                     <source src="/assets/Soulwhen.mp4" type="video/mp4"/>
@@ -30,7 +58,7 @@ const Edition = () => {
             </div>
         </div>
 
-        {/* 2ème édition  */}
+        {/* 3ème édition  */}
         <div className="editions">
         <div className='img' >
             <img style={{width:'30%'}} src="/assets/Affiche Molo.jpg" alt="Loconada"/>
@@ -44,8 +72,7 @@ const Edition = () => {
             <img style={{width:'30%'}} src="/assets/Molo-8.jpg" alt="Loconada"/>
             </div>
             <div className="edition-content">
-                <h2>Loconada 2ème édition</h2>
-                <p>La deuxième édition Loconada s'est déroulée le 09 septembre 2024 au Molo une boite de nuit située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
+                <p>Cette édition Loconada s'est déroulée le 09 septembre 2024 au Molo une boite de nuit située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
                 LINEUP : <br/> - Annabi <br/> - Izma <br/> - Ghassen Azaiez <br/> - Anis Houissa <br/> - Astrid <br/> - Saif Touati </p>
                 <video controls>
                     <source src="/assets/Molo.mp4" type="video/mp4"/>
@@ -54,7 +81,7 @@ const Edition = () => {
             </div>
         </div>
 
-        {/* 3ème édition  */}
+        {/* 4ème édition  */}
         <div className="editions">
             <div className='img' >
             <img style={{width:'30%'}} src="/assets/Affiche Beatz.jpg" alt="Loconada"/>
@@ -64,8 +91,7 @@ const Edition = () => {
             <img style={{width:'30%'}} src="/assets/4.jpg" alt="Loconada"/>
             </div>
             <div className="edition-content">
-                <h2>Loconada 3ème édition</h2>
-                <p>La troisème édition Loconada s'est déroulée le 29 novembre 2024 au Beatz Club une boite de nuit située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
+                <p>Cette édition Loconada s'est déroulée le 29 novembre 2024 au Beatz Club une boite de nuit située à Gammarth. Avec un lineup de folie qui contient des DJs talentueux specialistes en Afrohouse et Afroteck. <br/> 
                 LINEUP : <br/> - Daly B2B K-mir <br/> - Faruk Khaledi <br/> - Ghassen Azaiez <br/> - Anis Houissa <br/> - Rain <br/> - Shady B2B Mzid <br/> - Zus </p>
                 <video controls>
                     <source src="/assets/Beatz.mp4" type="video/mp4"/>
